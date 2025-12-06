5509cc4bf2e5a011115a067cc5d1148a99193d98
milesfodor <milesfodor@comcast.net>
2025-12-06 18:10:49 -0500

    Changed inline comments

--- src/CountCharactersAndLines.java
+++ src/CountCharactersAndLines.java
@@ -6,7 +6,6 @@ import java.io.File;
  */
 public class CountCharactersAndLines {
     public static void main(String args[]) throws Exception {
-        // HtmlImageUtil.java is available to this program
         String filename = "src/MinMaxIntField.java";
         int lineCount = 0;
         int charCount = 0;
@@ -14,8 +13,8 @@ public class CountCharactersAndLines {
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             String str = scanner.nextLine();
-            charCount += str.length();
-            lineCount++;
+            charCount += str.length(); // Add each character in each string to charCount
+            lineCount++; // Increment line count each time we scan a line
         }
         System.out.println("No. of characters: " + charCount);
         System.out.println("No. of lines: " + lineCount);
